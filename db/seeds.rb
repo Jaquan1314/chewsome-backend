32b449e25a6a0c295e77f89370c1d8780e0f5c3e
Jaquan1314 <javaughngrant@gmail.com>
2021-01-14 10:58:54 -0500

    added a few dummy data to test out api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Favorite.destroy_all
+Review.destroy_all
+Restaurant.destroy_all
+User.destroy_all
+
+puts 'creating users 👨‍👩‍👧‍👦 '
+
+user_arr = [
+  {
+    username: 'Twin',
+    email: 'abc@abc',
+    password: 'abc123'
+
+  },
+  {
+    username: 'Dianna',
+    email: 'dian@dian',
+    password: 'abc123'
+  },
+  {
+    username: 'Renee',
+    email: 'ren@ren',
+    password: 'abc123'
+  },
+]
+
+user_arr.each do |uesrObj|
+  User.create!(uesrObj)
+end
