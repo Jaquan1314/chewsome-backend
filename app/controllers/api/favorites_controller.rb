e38d27b56ea9fd6ac3dffc6bde116f2e76c726e0
Jaquan1314 <javaughngrant@gmail.com>
2021-01-21 14:05:32 -0500

    added favorite controller and actions needed

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -1,2 +1,30 @@
 class Api::FavoritesController < ApplicationController
+  def index
+    favorites = Favorite.all
+    render json: favorites
+  end
+
+  def show 
+    favorite = Favorite.find(params[:id])
+    render json: favorite
+  end
+
+  def create
+    favorite = Favorite.create!(favorite_params)
+    if favorite.valid?
+      render json: favorite
+    else
+      render json: { error: 'failed to create favorite' }
+    end
+  end
+
+  def destroy
+    favorite = Favorite.find(params[:id])
+    favorite.destroy!
+    render json: {}
+  end
+
+  def favorite_params
+    params.permit(:user_id, :restaurant_id)
+  end
 end
