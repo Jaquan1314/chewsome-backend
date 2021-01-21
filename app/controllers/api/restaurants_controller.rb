62c3efdf73f32ae87b55f8380201ddc628ecb3f5
Jaquan1314 <javaughngrant@gmail.com>
2021-01-20 22:28:14 -0500

    commented out external api fetch

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -2,12 +2,13 @@
 class Api::RestaurantsController < ApplicationController
   def index
     # byebug
-    response = RestClient::Request.execute(
-      method: "GET",
-      url: "https://api.yelp.com/v3/businesses/search?term=delis&location=nyc&sort_by=rating",
-      headers: { Authorization: "Bearer #{ENV['YELP_API_KEY']}" },
-    )
-    restaurants = JSON.parse(response)
+    restaurants = Restaurant.all
+    # response = RestClient::Request.execute(
+    #   method: "GET",
+    #   url: "https://api.yelp.com/v3/businesses/search?term=delis&location=nyc&sort_by=rating",
+    #   headers: { Authorization: "Bearer #{ENV['YELP_API_KEY']}" },
+    # )
+    # restaurants = JSON.parse(response)
     render json: restaurants
   end
 
